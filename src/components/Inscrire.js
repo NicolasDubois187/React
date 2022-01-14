49bd8632af03892034ad054a7842d028a6dbf91d
NicolasDubois187 <nicolasdubois187.pro@gmail.com>
2022-01-14 22:18:50 +0100

    cours-React-1

--- src/components/Inscrire.js
+++ src/components/Inscrire.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Inscrire = () => {
+    const [password, setPassword] = useState("");
+    const [confirmPass, setConfirmPass] = useState("");
+
     return (
         <div className='inscription'>
             <h2>Inscrivez-vous</h2>
             <label htmlFor="pseudo">Pseudo</label>
             <input type="text" id="pseudo" />
             <label htmlFor="mdp">Mot de passe</label>
-            <input type="text" id="mdp" />
+            <input type="password" id="mdp" onChange={(e) => setPassword(e.target.value)} />
+            {password.length < 8 && password.length > 3 && (
+        <p>Le mot de passe doit être de 8 caractères minimum</p>
+            )}
             <label htmlFor="confirmMdp">Confirmer mot de passe</label>
-            <input type="text" id="confirmMdp" />
-            <input type="submit" id="submit" />
+            <input type="password" id="confirmMdp" onChange={(e) => setConfirmPass(e.target.value)} />
+            {password !== confirmPass && confirmPass.length > 3 && (
+        <p>Les mots de passe ne correspondent pas</p>
+      )}
+            <input type="submit" id="submit" value="Valider" />
         </div>
     );
 };
